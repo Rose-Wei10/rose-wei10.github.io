86876efab5e93337882dec5653175df390119f5f
Rose-Wei10 <weir0210@gmail.com>
2026-05-01 03:15:31 -0700

    Delete Rose_Portfolio-main/src directory

--- Rose_Portfolio-main/src/email.d.ts
+++
@@ -1,7 +0,0 @@
-// src/email.d.ts
-interface Window {
-    emailjs: {
-      init: (userId: string) => void;
-      send: (serviceId: string, templateId: string, templateParams: any, userId: string) => Promise<any>;
-    }
-  }
